8885e8ffe8be4426b2093d421c9b8fe0ddc322d8
Florin <florinmihu31@gmail.com>
2023-01-19 09:42:13 +0200

    Implement sending the payment transaction

--- app/js/app.js
+++ app/js/app.js
@@ -1,42 +1,57 @@
-import { Transaction } from "@multiversx/sdk-core";
+import { Account, Transaction } from "@multiversx/sdk-core";
 import { Address } from "@multiversx/sdk-core";
 import { UserSecretKey } from "@multiversx/sdk-wallet";
 import { UserSigner } from "@multiversx/sdk-wallet";
+import { TransactionPayload } from "@multiversx/sdk-core";
+import { ProxyNetworkProvider } from "@multiversx/sdk-network-providers";
+import { TokenPayment } from "@multiversx/sdk-core";
 // Import the file system module
 //const fs = require("fs");
-import { fs } from "fs";
+// import { fs } from "fs";
 
 // PEM file content
-// var pem = "-----BEGIN PRIVATE KEY for erd1uy5gsjpud3jdyt6c2udhxj06d644dv28q43trg3agme6hhpcjpgs5d6szv-----\n" + 
-// "NjZiNWM5YTc3N2NlZDViYjNjZTExMWM3ZGNlZGRkM2JmOWE3ZTEyOTAyODUwZDQw\n" +
-// "MDhhNjVhZWM3ZDUzZGFjMmUxMjg4ODQ4M2M2YzY0ZDIyZjU4NTcxYjczNDlmYTZl\n" +
-// "YWI1NmIxNDcwNTYyYjFhMjNkNDZmM2FiZGMzODkwNTE=\n" +
-// "-----END PRIVATE KEY for erd1uy5gsjpud3jdyt6c2udhxj06d644dv28q43trg3agme6hhpcjpgs5d6szv-----"
-const pemText = fs.readFileSync("/home/student/Documents/Wallet/wallet.pem", { encoding: "utf8" });
+var pem = "TODO: REPLACE WITH YOUR PEM FILE CONTENT"
+
 
 // Generate new user secret key from the PEM string
 const userSecretKey = UserSecretKey.fromPem(pem);
 
 // Get the address of the user secret key
 const address = userSecretKey.generatePublicKey().toAddress();
+console.log("Address", address.bech32());
 
 const userSigner = new UserSigner(userSecretKey);
+const networkProvider = new ProxyNetworkProvider("https://devnet-gateway.elrond.com");
 
-// Send the transaction
-const transaction = new Transaction({
-    nonce: 0,
-    value: "0",
-    receiver: Address.fromBech32("erd1qqqqqqqqqqqqqpgqknatrzgys8rmknnjxrn0vjue5sc5g4gnjpgsjeknhz"),
-    sender: address,
-    gasPrice: 1000000000,
-    gasLimit: 2000000,
-    data: "pay",
-    chainID: "D",
-});
+const client = new Account(address);
+const clientOnNetwork = await networkProvider.getAccount(address);
+client.update(clientOnNetwork);
 
-// Sign the transaction
-await userSigner.sign(transaction);
-console.log("Transaction signature", transaction.getSignature().hex());
-console.log("Transaction hash", transaction.getHash().hex());
+console.log("Account", client);
 
+// sendTransaction();
 
+async function sendTransaction() {
+    // Send the transaction
+    const transaction = new Transaction({
+        nonce: client.getNonceThenIncrement(),
+        value: TokenPayment.egldFromBigInteger(1000000000000000000n),
+        receiver: Address.fromBech32("erd1qqqqqqqqqqqqqpgqknatrzgys8rmknnjxrn0vjue5sc5g4gnjpgsjeknhz"),
+        sender: address,
+        gasPrice: 1000000000,
+        gasLimit: 2000000,
+        data: new TransactionPayload(Buffer.from("Hello world!", "utf8")),
+        chainID: "D",
+    });
+
+    console.log("Transaction", transaction.toPlainObject());
+
+    // Sign the transaction
+    await userSigner.sign(transaction);
+    console.log("Transaction signature", transaction.getSignature().hex());
+    console.log("Transaction hash", transaction.getHash().hex());
+
+    // Send the transaction
+    const transactionHash = await networkProvider.sendTransaction(transaction);
+    console.log("Transaction hash", transactionHash);
+}
